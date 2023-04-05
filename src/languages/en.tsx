c99b83d88fe8f834e5fd1713762d5e47a91f6a06
rebeccayourstone <annarebeccasvensson@gmail.com>
2023-04-05 21:20:51 +0200

    Add translation for main page.

--- src/languages/en.tsx
+++ src/languages/en.tsx
@@ -1,3 +1,10 @@
 export default {
   hello: "Hello",
+  myName: "My name is Rebecca Yourstone and i study Front end development at ",
+  school: "EC in Helsingborg, Sweden",
+  searching: "I'm looking for an internship",
+  preferdWork:
+    "I prefer to build using TypeScript and React, i am however open to use other frameworks or libraries such as Angular or vanilla JavaScript",
+  builtWith:
+    "This site is built and designed by me using TypeScript and React with styling using Bootstrap and CSS",
 } as { [p: string]: string };
--- src/languages/sv.tsx
+++ src/languages/sv.tsx
@@ -1,3 +1,11 @@
 export default {
   hello: "Hej",
+  myName:
+    "Mitt namn är Rebecca Yourstone fd Svensson och jag läser till Frontendutvecklare på ",
+  school: "EC i Helsingborg.",
+  searching: "söker jag LIA-plats inom Frontendutveckling!",
+  preferdWork:
+    "Jag bygger helst i TypeScript och React, men är öppen för andra ramverk/bibliotek så som Angular eller ren JavaScript.",
+  builtWith:
+    "Denna sida är byggd och designad av mig i TypeScript och React med style i Bootstrap och CSS.",
 } as { [p: string]: string };
